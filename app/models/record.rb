659e290abb2ba51479dc57598e67b98bd329148b
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2024-04-17 17:40:26 -0700

    Validations

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,6 @@
 class Record < ApplicationRecord
+    validates :title, presence: true
+    validates :artist, presence: true
+    validates :image_url, presence: true
+    validates :release_date, presence: true
 end
